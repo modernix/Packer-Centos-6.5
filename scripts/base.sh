eda65fa6980bec89166ee1950a359be805f27e0a
Mike Beauchamp <beauchompers@gmail.com>
2014-03-01 22:16:40 -0700

    Adding tty for vagrant
    adding tty for vagrant

--- scripts/base.sh
+++ scripts/base.sh
@@ -9,6 +9,9 @@ yum -y install gcc make curl wget sudo perl fuse-libs nfs-utils
 echo "Updating packages..."
 yum -y update
 
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+sed -i "s/^\(.*env_keep = \"\)/\1PATH /" /etc/sudoers
+
 echo "Rebooting the machine"
 reboot
 sleep 30
